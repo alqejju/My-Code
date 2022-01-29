ef4d64a49819ef056ca37daedfa4d49ea60c2b3e
alqejju <Alqejju@gmail.com>
2022-01-30 03:50:25 +0700

    Recursion

---
+++ Recursion/Factorization/main.cpp
@@ -0,0 +1,25 @@
+// Factorial of n = 1*2*3*...*n
+
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int main() {
+    int n;
+
+    cout << "Enter a non-negative number: ";
+    cin >> n;
+
+    
+    cout << "Factorial of " << n << " = " << factorial(n);
+    return 0;
+}
+
+int factorial(int n) {
+    if (n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+}
