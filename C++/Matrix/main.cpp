f8c2ab13521c0e0c526b4ec025ffb47e103dcdb0
alqejju <Alqejju@gmail.com>
2022-03-07 15:54:10 +0700

    matrix

--- C++/Matrix/main.cpp
+++ C++/Matrix/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    
+    int column,row,parameter = 0,parameterEnd = 0;
+    cout << "Input your Matrix A row size : ";cin >> row; cout << "Input your Matrix A column size : ";cin >> column;
+    int number[column][row];
+
+    cout << endl << "Matrix A = " << endl;
+    for (int a = 0; a < column; a++)
+    {
+        for (int b = 0; b < row; b++)
+        {
+            cout << "[ "; cin >> setw(3) >> right >> number[a][b];system("CLS");
+            parameter++;
+            for (int c = 0; c < column; c++)
+            {
+                for (int d = 0; d < row; d++)
+                {
+                    cout << "[";cout << setw(3) << right << number[c][d] << "]";parameterEnd++;
+                    if (d == row-1)
+                        {
+                            cout << endl;
+                        }
+                    if (parameter == parameterEnd)
+                        {
+                            break;
+                        }
+                }
+                if (parameterEnd == parameter)
+                    {
+                        break;
+                    }
+            }parameterEnd = 0; 
+        } 
+    }
+
     return 0;
 }
