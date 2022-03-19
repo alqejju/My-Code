73889057ce6a0088bd0327788e2748b54a31791c
alqejju <Alqejju@gmail.com>
2022-03-19 00:43:55 -0700

    Java

--- C++/Matrix/main.cpp
+++ C++/Matrix/main.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+class number
+{
+
+public:
+    int indexX,indexY;
+    int value;
+
+    void insert(int num){
+        num;
+    }
+    
+
+};
+
+number::number(/* args */)
+{
+}
+
+number::~number()
+{
+}
+
+
 int main()
 {
     
+
     return 0;
 }
---
+++ Java/Belajar/belajar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class belajar {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        System.out.print("Your Variable is : ");;System.out.println(a);
+        Scanner input = new Scanner(System.in);
+
+        int b = input.nextInt();
+        System.out.print("Your Input Variable is : ");;System.out.print(b);
+
+        input.close();
+    }
+    
+}
